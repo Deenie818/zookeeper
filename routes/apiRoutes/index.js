0483ea8f7cacd82e7247b66d610d80941771c3d3
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-13 21:28:33 -0700

    front end functionality for zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,7 @@ const animalRoutes = require('../apiRoutes/animalRoutes');
 
 router.use(animalRoutes);
 
+router.use(require('./zookeeperRoutes'));
+
 module.exports = router;
 
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -17,4 +17,6 @@ router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+
+
 module.exports = router;
